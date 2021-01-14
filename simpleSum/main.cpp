7058d6705028d8e010beb3cd1c15ae7b6155a173
João Nicastro <77421792+9pa@users.noreply.github.com>
2021-01-14 00:22:14 -0300

    uri judge problems

---
+++ simpleSum/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    int A, B, X; //declaração de variáveis 
+
+    cin>>A; //atribuição de variáveis 
+    cin>>B; //atribuição de variáveis 
+
+    X = A + B; //atribuição de variáveis 
+
+    cout<<"X = "<<X<<endl;
+
+
+    return 0;
+}
